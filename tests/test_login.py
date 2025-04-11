d85fec24a4e3f8e6d43236d9fd39ab75fa294b85
Your Name <you@example.com>
2025-04-11 08:34:20 +0300

    Updated tests and added new tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,10 +1,11 @@
-from pages.login_page import Alert, LoginPage
+from pages.login_page import LoginPage
 
 
 def test_login_valid_credentials(test_driver, user_credentials, base_url):
     login_page = LoginPage(test_driver)
     test_driver.get(base_url)
 
+    login_page.get_log_in_modal()
     login_page.login(
         username=user_credentials.username, password=user_credentials.password
     )
@@ -17,6 +18,7 @@ def test_log_out(test_driver, user_credentials, base_url):
     login_page = LoginPage(test_driver)
     test_driver.get(base_url)
 
+    login_page.get_log_in_modal()
     login_page.login(
         username=user_credentials.username, password=user_credentials.password
     )
@@ -31,27 +33,56 @@ def test_log_out(test_driver, user_credentials, base_url):
 
 def test_login_with_missing_credentials(test_driver, base_url):
     login_page = LoginPage(test_driver)
-    login_alert = Alert(test_driver)
     test_driver.get(base_url)
 
     login_page.get_log_in_modal()
     login_page.click_log_in()
 
-    alert_text = login_alert.get_alert_text()
+    alert_text = login_page.get_alert_text()
 
     assert "Please fill out Username and Password." in alert_text
 
 
 def test_login_with_wrong_password(test_driver, base_url, user_credentials):
     login_page = LoginPage(test_driver)
-    login_alert = Alert(test_driver)
     test_driver.get(base_url)
 
     login_page.get_log_in_modal()
-    login_page.set_username(user_credentials.username)
-    login_page.set_password("fake password")
-    login_page.click_log_in()
+    login_page.login(username=user_credentials.username, password="fake password")
 
-    alert_text = login_alert.get_alert_text()
+    alert_text = login_page.get_alert_text()
 
     assert "Wrong password" in alert_text
+
+
+def test_login_with_inexistent_user(test_driver, base_url):
+    login_page = LoginPage(test_driver)
+    test_driver.get(base_url)
+
+    login_page.get_log_in_modal()
+    login_page.login(username="lsfjdsjdfosidufksmdfls!!!!", password="fake password")
+
+    alert_text = login_page.get_alert_text()
+
+    assert "User does not exist" in alert_text
+
+
+def test_successful_login_after_initial_failed_attempt(
+    test_driver, base_url, user_credentials
+):
+    login_page = LoginPage(test_driver)
+    test_driver.get(base_url)
+
+    login_page.get_log_in_modal()
+    login_page.login(username="lsfjdsjdfosidufksmdfls!!!!", password="fake password")
+    login_page.accept_alert()
+
+    login_page.clear_username_box()
+    login_page.clear_password_box()
+
+    login_page.login(
+        username=user_credentials.username, password=user_credentials.password
+    )
+    logged_in_user = login_page.get_logged_in_user()
+
+    assert logged_in_user.text == f"Welcome {user_credentials.username}"
