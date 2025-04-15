89ce604068d3be08efae361df490dc814190a577
Your Name <you@example.com>
2025-04-15 08:44:48 +0300

    Removed logic that better fits in Navbar class from LoginPage class

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,11 +1,14 @@
 from pages.login_page import LoginPage
+from pages.navbar_page import NavbarPage
 
 
 def test_login_valid_credentials(test_driver, user_credentials, base_url):
     login_page = LoginPage(test_driver)
+    navbar_page = NavbarPage(test_driver)
     test_driver.get(base_url)
 
-    login_page.get_log_in_modal()
+    log_in_button = navbar_page.get_log_in_button()
+    login_page.get_log_in_modal(log_in_button)
     login_page.login(
         username=user_credentials.username, password=user_credentials.password
     )
@@ -16,9 +19,11 @@ def test_login_valid_credentials(test_driver, user_credentials, base_url):
 
 def test_log_out(test_driver, user_credentials, base_url):
     login_page = LoginPage(test_driver)
+    navbar_page = NavbarPage(test_driver)
     test_driver.get(base_url)
 
-    login_page.get_log_in_modal()
+    log_in_button = navbar_page.get_log_in_button()
+    login_page.get_log_in_modal(log_in_button)
     login_page.login(
         username=user_credentials.username, password=user_credentials.password
     )
@@ -28,14 +33,16 @@ def test_log_out(test_driver, user_credentials, base_url):
 
     login_page.log_out()
 
-    assert login_page.is_element_visible(login_page.NAVIGATION_LOG_IN)
+    assert navbar_page.is_element_visible(navbar_page.LOG_IN_BUTTON)
 
 
 def test_login_with_missing_credentials(test_driver, base_url):
     login_page = LoginPage(test_driver)
+    navbar_page = NavbarPage(test_driver)
     test_driver.get(base_url)
 
-    login_page.get_log_in_modal()
+    log_in_button = navbar_page.get_log_in_button()
+    login_page.get_log_in_modal(log_in_button)
     login_page.click_log_in()
 
     alert_text = login_page.get_alert_text()
@@ -45,9 +52,11 @@ def test_login_with_missing_credentials(test_driver, base_url):
 
 def test_login_with_wrong_password(test_driver, base_url, user_credentials):
     login_page = LoginPage(test_driver)
+    navbar_page = NavbarPage(test_driver)
     test_driver.get(base_url)
 
-    login_page.get_log_in_modal()
+    log_in_button = navbar_page.get_log_in_button()
+    login_page.get_log_in_modal(log_in_button)
     login_page.login(username=user_credentials.username, password="fake password")
 
     alert_text = login_page.get_alert_text()
@@ -57,9 +66,11 @@ def test_login_with_wrong_password(test_driver, base_url, user_credentials):
 
 def test_login_with_inexistent_user(test_driver, base_url):
     login_page = LoginPage(test_driver)
+    navbar_page = NavbarPage(test_driver)
     test_driver.get(base_url)
 
-    login_page.get_log_in_modal()
+    log_in_button = navbar_page.get_log_in_button()
+    login_page.get_log_in_modal(log_in_button)
     login_page.login(username="lsfjdsjdfosidufksmdfls!!!!", password="fake password")
 
     alert_text = login_page.get_alert_text()
@@ -71,9 +82,11 @@ def test_successful_login_after_initial_failed_attempt(
     test_driver, base_url, user_credentials
 ):
     login_page = LoginPage(test_driver)
+    navbar_page = NavbarPage(test_driver)
     test_driver.get(base_url)
 
-    login_page.get_log_in_modal()
+    log_in_button = navbar_page.get_log_in_button()
+    login_page.get_log_in_modal(log_in_button)
     login_page.login(username="lsfjdsjdfosidufksmdfls!!!!", password="fake password")
     login_page.accept_alert()
 
