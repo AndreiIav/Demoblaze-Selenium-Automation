24a4e4f964fa78e9e9bdf47fa98f9ca434bca0c0
Your Name <you@example.com>
2025-05-18 10:02:17 +0300

    Refactored LoginPage methods to use explicit waits

--- tests/pages/login_page.py
+++ tests/pages/login_page.py
@@ -16,30 +16,33 @@ class LoginPage(BasePage):
 
     def get_log_in_modal(self, log_in_button):
         log_in_button.click()
-        self.is_element_visible(self.LOG_IN_MODAL)
+        if self.check_if_element_is_visible(self.LOG_IN_MODAL):
+            return True
+        else:
+            raise LookupError("Log_in modal not present")
 
     def set_username(self, username):
-        username_box = self.driver.find_element(*self.USERNAME)
+        username_box = self.get_element(locator=self.USERNAME)
         username_box.send_keys(username)
 
     def set_password(self, password):
-        password_box = self.driver.find_element(*self.PASSWORD)
+        password_box = self.get_element(locator=self.PASSWORD)
         password_box.send_keys(password)
 
     def click_log_in(self):
-        log_in_button = self.driver.find_element(*self.MODAL_LOG_IN_BUTTON)
+        log_in_button = self.get_element(self.MODAL_LOG_IN_BUTTON)
         log_in_button.click()
 
     def clear_username_box(self):
-        username_box = self.driver.find_element(*self.USERNAME)
+        username_box = self.get_element(locator=self.USERNAME)
         username_box.clear()
 
     def clear_password_box(self):
-        password_box = self.driver.find_element(*self.PASSWORD)
+        password_box = self.get_element(locator=self.PASSWORD)
         password_box.clear()
 
     def get_logged_in_user(self):
-        self.is_text_present_in_element(
+        self.check_if_text_is_present_in_element(
             locator=(self.LOGGED_IN_USER), text=self.WELCOME_USER_TEXT
         )
         logged_in_user = self.driver.find_element(*self.LOGGED_IN_USER)
@@ -52,7 +55,7 @@ class LoginPage(BasePage):
         self.click_log_in()
 
     def log_out(self):
-        log_out_button = self.driver.find_element(*self.LOG_OUT)
+        log_out_button = self.get_element(self.LOG_OUT)
         log_out_button.click()
 
     def get_alert_text(self):
