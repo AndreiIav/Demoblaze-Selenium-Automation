d9d9cd29e5a1357b0e5de1ab68d58579a0b0c946
Your Name <you@example.com>
2025-07-15 08:22:43 +0300

    Removed option to select browser

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,14 +8,6 @@ from utils.data_loader import parse_products_json_data
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--browser",
-        action="store",
-        default="firefox",
-        help="Browser to run tests (chrome/firefox)",
-        choices=("chrome", "firefox"),
-    )
-
-    parser.addoption(
         "--headless", action="store_true", help="Run browser in headless mode"
     )
 
